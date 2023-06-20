786fa3e2491c10d89dbb913589e4c6da71a1d2c9
Hacksy <hansy.schmitt.dev@gmail.com>
2023-06-19 23:11:11 -0500

    Add Placeholder Home Page

---
+++ lib/presentation/features/home/home_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:pokedex/presentation/features/home/home_screen.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const HomeScreen();
+  }
+}
