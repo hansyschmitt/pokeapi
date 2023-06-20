9b809fd3f8fa7e22c1ab9d89e9e24e4e2d8eabda
Hansy Schmitt <hansy.schmitt.dev@gmail.com>
2023-06-19 23:11:44 -0500

    Add Placeholder Details Page

---
+++ lib/presentation/features/detail/detail_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:pokedex/presentation/features/detail/detail_screen.dart';
+
+class DetailPage extends StatelessWidget {
+  const DetailPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const DetailScreen();
+  }
+}
